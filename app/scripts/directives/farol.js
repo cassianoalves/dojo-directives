cefbf14c56de14d9f17b3f12da0b4a8e9f073b6e
Cassiano Alves <cassiano.alves@b2wdigital.com>
2015-02-05 20:40:26 -0200

    Exemplo implementado

--- app/scripts/directives/farol.js
+++ app/scripts/directives/farol.js
@@ -9,10 +9,22 @@
 angular.module('dojoDirectiveApp')
   .directive('farol', function () {
     return {
-      template: '<div></div>',
-      restrict: 'E',
-      link: function postLink(scope, element, attrs) {
-        element.text('this is the farol directive');
+      template: '<div class="{{ \'fa fa-circle fa-4x \' + cor }}" ></div>',
+      restrict: 'A',
+      scope: {
+        valor: '=farol'
+      },
+      link: function postLink($scope) {
+
+        $scope.$watch('valor', function () {
+          if($scope.valor < 20) {
+            $scope.cor='text-danger';
+          } else if($scope.valor > 80) {
+            $scope.cor='text-success';
+          } else {
+            $scope.cor='text-warning';
+          }
+        });
       }
     };
   });
