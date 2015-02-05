37fa55acd61688c0b3487aea3ebaff3d789c67cf
Cassiano Alves <cassiano.alves@b2wdigital.com>
2015-02-05 20:43:34 -0200

    Usando novo escopo, conforme modelo do teste do yo

--- test/spec/directives/farol.js
+++ test/spec/directives/farol.js
@@ -9,7 +9,7 @@ describe('Directive: farol', function () {
     scope;
 
   beforeEach(inject(function ($rootScope) {
-    scope = $rootScope;
+    scope = $rootScope.$new();
   }));
 
   it('should make hidden element visible', inject(function ($compile) {
