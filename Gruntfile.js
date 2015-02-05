17e579983f25dd63ca8e2bbde0bac8276c8e4009
Cassiano Alves <cassiano.alves@b2wdigital.com>
2015-02-05 18:35:44 -0200

    Adicionando pacotes do Karma que não vinham no modelo do yo

--- Gruntfile.js
+++ Gruntfile.js
@@ -320,6 +320,13 @@ module.exports = function (grunt) {
           cwd: 'bower_components/bootstrap/dist',
           src: 'fonts/*',
           dest: '<%= yeoman.dist %>'
+        },{
+          //for font-awesome
+          expand: true,
+          dot: true,
+          cwd: 'bower_components/fontawesome',
+          src: ['fonts/*.*'],
+          dest: '<%= yeoman.dist %>'
         }]
       },
       styles: {
